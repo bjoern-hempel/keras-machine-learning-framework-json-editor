c3c5081ff48738d25b346ae102ee17b95a540f7a
Björn Hempel <bjoern@hempel.li>
2019-11-16 17:08:07 +0100

    refactoring

--- js/main.js
+++ js/main.js
@@ -11,3 +11,61 @@ function loadJSON(jsonPath, callback)
     };
     xobj.send(null);
 }
+
+function saveJSON(jsonPath, callback)
+{
+
+}
+
+function startEditor()
+{
+    loadJSON('data/ml.json', function(response) {
+        let json_data = JSON.parse(response);
+
+        // Initialize the editor
+        let editor = new JSONEditor(document.getElementById('editor_holder'),{
+            // Enable fetching schemas via ajax
+            ajax: true,
+
+            // The schema for the editor
+            schema: {
+                $ref: "json/ml.json",
+                format: "grid"
+            },
+
+            // Seed the form with a starting value
+            startval: json_data
+        });
+
+        // Hook up the submit button to log to the console
+        document.getElementById('submit').addEventListener('click',function() {
+            // Get the value from the editor
+            console.log(editor.getValue());
+        });
+
+        // Hook up the Restore to Default button
+        document.getElementById('restore').addEventListener('click',function() {
+            editor.setValue(json_data);
+        });
+
+        // Hook up the validation indicator to update its
+        // status whenever the editor changes
+        editor.on('change',function() {
+            // Get an array of errors from the validator
+            var errors = editor.validate();
+
+            var indicator = document.getElementById('valid_indicator');
+
+            // Not valid
+            if(errors.length) {
+                indicator.className = 'label alert';
+                indicator.textContent = 'not valid';
+            }
+            // Valid
+            else {
+                indicator.className = 'label success';
+                indicator.textContent = 'valid';
+            }
+        });
+    });
+}
